549d9f192f78d6e1326fffe5e8d4615f6807588e
Fadil <76504231+fadil54@users.noreply.github.com>
2022-12-11 08:39:27 +0700

    Add files via upload
    New

---
+++ UTS_NO2_C_3090_Baru/src/app/ProjectPlanner_3090.java
@@ -0,0 +1,27 @@
+package app;
+
+public class ProjectPlanner_3090 {
+    private String nama_3090, nip_3090;
+    private int gajiPokok_3090, komisi_3090, totalHasilProyek_3090;
+    
+    public ProjectPlanner_3090(String nama_3090, String nip_3090, int gajiPokok_3090, int komisi_3090, int totalHasilProyek_3090){
+        this.nama_3090 = nama_3090;
+        this.nip_3090 = nip_3090;
+        this.gajiPokok_3090 = gajiPokok_3090;
+        this.komisi_3090 = komisi_3090;
+        this.totalHasilProyek_3090 = totalHasilProyek_3090;
+    }
+    public int hitungGaji(){
+        return gajiPokok_3090 + (komisi_3090 * totalHasilProyek_3090);
+    }
+
+    public void info(){
+        System.out.println("===Project Planner===");
+        System.out.println("Nama_3090               : " + nama_3090);
+        System.out.println("Nip_3090                : " + nip_3090);
+        System.out.println("Gaji Pokok              : " + gajiPokok_3090);
+        System.out.println("Komisi_3090             : " + komisi_3090);
+        System.out.println("Total Hasil Proyek      : " + totalHasilProyek_3090);
+        System.out.println("Total Gaji              : " + hitungGaji());
+    }
+}
---
+++ UTS_NO2_C_3090_Baru/src/app/SalariedEmployee_3090.java
@@ -0,0 +1,23 @@
+package app;
+
+public class SalariedEmployee_3090{
+    private String nama_3090, nip_3090;
+    private int upahMingguan_3090;
+
+    public SalariedEmployee_3090(String nama_3090, String nip_3090, int upahMingguan_3090){
+        this.nama_3090 = nama_3090;
+        this.nip_3090 = nip_3090;
+        this.upahMingguan_3090 = upahMingguan_3090;
+    }
+    public int hitungGaji(){
+        return upahMingguan_3090;
+    }
+
+    public void info(){
+        System.out.println("===Salaried Employee===");
+        System.out.println("Nama_3090               : " + nama_3090);
+        System.out.println("Nip_3090                : " + nip_3090);
+        System.out.println("Upah Mingguan           : " + upahMingguan_3090);
+        System.out.println("Total Gaji              : " + hitungGaji());
+    }
+}
